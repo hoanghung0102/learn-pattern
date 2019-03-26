efeb66f19fc0046de68bac2b3c94bb48a922e214
Hung Hoang <hung.hoang@axonactive.com>
2019-03-26 14:57:02 +0700

    Ex: Singleton creation

--- src/main/java/hoanghung/com/demo/singleton/TestSingleTon.java
+++ src/main/java/hoanghung/com/demo/singleton/TestSingleTon.java
@@ -5,6 +5,7 @@ public class TestSingleTon {
     public static void main(String... args) {
         SingleTonCls singleTonCls1 = SingleTonCls.getInstance();
         SingleTonCls singleTonCls2 = SingleTonCls.getInstance();
+
         System.out.println("Has code = " + singleTonCls1.hashCode());
         System.out.println("Has code = " + singleTonCls2.hashCode());
 
